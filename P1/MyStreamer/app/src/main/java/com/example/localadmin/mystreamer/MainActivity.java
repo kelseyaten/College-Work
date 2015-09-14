90a904791103aaf87f06a48449d59f1ec2296ee4
kex496 <kex496@aa21778b-74f5-4dbf-b9af-9d949791c513>
2015-09-14 23:40:36 +0000

    Import MyStreamer

---
+++ P1/MyStreamer/app/src/main/java/com/example/localadmin/mystreamer/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.localadmin.mystreamer;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Album;
+import kaaes.spotify.webapi.android.models.Artist;
+import kaaes.spotify.webapi.android.models.ArtistsPager;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        Picasso.with(this)
+                .load("http://i.imgur.com/s6Nkq0c.png")
+                .into(imageView);
+
+        final SpotifyApi api = new SpotifyApi();
+        final SpotifyService spotify = api.getService();
+
+        final SearchView searchView = (SearchView) findViewById(R.id.searchView);
+        searchView.setBackgroundColor(Color.LTGRAY);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+
+                spotify.searchArtists(s, new Callback<ArtistsPager>() {
+                    @Override
+                    public void success(ArtistsPager artistsPager, Response response) {
+                        int count = 0;
+                            for( Artist artist: artistsPager.artists.items ) {
+                                if(count < 10) {
+                                    Log.d("ArtistName", artist.name);
+                                    count++;
+                                }
+                            }
+                    }
+
+                    @Override
+                    public void failure(RetrofitError error) {
+                        Log.d("Artist Failure", error.getUrl() );
+                    }
+                });
+
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
