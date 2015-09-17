9dd2d4dc8018b7ef80b9cc4ecfb81aa6c954a0b1
kex496 <kex496@aa21778b-74f5-4dbf-b9af-9d949791c513>
2015-09-17 02:35:13 +0000

    New Activity W/ button

--- P1/MyStreamer/app/src/main/java/com/example/localadmin/mystreamer/MainActivity.java
+++ P1/MyStreamer/app/src/main/java/com/example/localadmin/mystreamer/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.localadmin.mystreamer;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
@@ -8,7 +9,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SearchView;
@@ -16,16 +19,6 @@ import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
-import java.util.ArrayList;
-
-import kaaes.spotify.webapi.android.SpotifyApi;
-import kaaes.spotify.webapi.android.SpotifyService;
-import kaaes.spotify.webapi.android.models.Album;
-import kaaes.spotify.webapi.android.models.Artist;
-import kaaes.spotify.webapi.android.models.ArtistsPager;
-import retrofit.Callback;
-import retrofit.RetrofitError;
-import retrofit.client.Response;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -37,59 +30,16 @@ public class MainActivity extends AppCompatActivity {
         Picasso.with(this)
                 .load("http://i.imgur.com/s6Nkq0c.png")
                 .into(imageView);
-        //final so that you can access the variable inside the method
-        final SpotifyApi api = new SpotifyApi();
-        final SpotifyService spotify = api.getService();
 
-        final SearchView searchView = (SearchView) findViewById(R.id.searchView);
-        searchView.setBackgroundColor(Color.LTGRAY);
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+        Button search;
+        search=(Button)findViewById(R.id.search);
+        search.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public boolean onQueryTextSubmit(String s) {
-
-                spotify.searchArtists(s, new Callback<ArtistsPager>() {
-                    @Override
-                    public void success(ArtistsPager artistsPager, Response response) {
-                        int count = 0;
-                        ArrayList<Artist> artistArray = new ArrayList<>();
-
-                            //count to ten so you only see 10 artist names
-                            for( Artist artist: artistsPager.artists.items ) {
-                                if(count < 10) {
-                                    artistArray.add(artist);
-                                    Log.d("ArtistName", artist.name);
-                                    count++;
-                                }
-                            }
-
-
-                        //----Make the listView----
-                        ListView listView = (ListView) findViewById(R.id.listView);
-                        Context context = getApplicationContext();
-                        ArrayList<String> artistList = new ArrayList<>();
-
-                        for (Artist artist: artistArray) {
-                            artistList.add(artist.name);
-                        }
-
-                        
-                        ArrayAdapter<String> adapter = new ArrayAdapter<>(context, android.R.layout.simple_list_item_1, artistList);
-                        listView.setAdapter(adapter);
-                        adapter.notifyDataSetChanged();
-                    }
-
-                    @Override
-                    public void failure(RetrofitError error) {
-                        Log.d("Artist Failure", error.getUrl() );
-                    }
-                });
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String s) {
-                return false;
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Intent i = new Intent(getApplicationContext(), Main2Activity.class);
+                startActivity(i);
             }
         });
 
